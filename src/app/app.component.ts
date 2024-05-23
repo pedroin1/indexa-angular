be344ee9c2043eeadeaa3e7c219e1501538317b6
triad-pedro-bastos <pedro.bastos@triadpesquisa.com.br>
2024-05-23 18:16:55 -0300

    Primeiro commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,20 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ContainerComponent } from './components/container/container.component';
+import { HeaderComponent } from './components/header/header.component';
+import { SeparatorComponent } from './components/separator/separator.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    ContainerComponent,
+    HeaderComponent,
+    SeparatorComponent,
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'indexa';
---
+++ src/app/components/container/container.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'main-container',
+  standalone: true,
+  imports: [],
+  templateUrl: './container.component.html',
+  styleUrl: './container.component.scss',
+})
+export class ContainerComponent {}
