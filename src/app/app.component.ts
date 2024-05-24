46c694d85baa3b626daea0422270ee309f3fa670
Pedro Luiz <pedroluizpb45@outlook.com>
2024-05-23 23:16:09 -0300

    Adiconando listagem de contatos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,7 @@ import { RouterOutlet } from '@angular/router';
 import { ContainerComponent } from './components/container/container.component';
 import { HeaderComponent } from './components/header/header.component';
 import { SeparatorComponent } from './components/separator/separator.component';
+import { ContatoComponent } from './components/contato/contato.component';
 
 @Component({
   selector: 'app-root',
@@ -12,10 +13,11 @@ import { SeparatorComponent } from './components/separator/separator.component';
     ContainerComponent,
     HeaderComponent,
     SeparatorComponent,
+    ContatoComponent,
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  title = 'indexa';
+  alfabeto: string = `abcdefghijklmnopqrstuvwxyz`;
 }
